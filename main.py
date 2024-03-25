fd0449923421b25d268b60ae96cb6e61b4ab3c51
Harrison Kim <kim.harr@northeastern.edu>
2024-03-25 19:21:38 -0400

    main.py

--- main.py
+++ main.py
@@ -1,39 +1,22 @@
 import pandas as pd
-from imblearn.under_sampling import RandomUnderSampler
+import numpy as np
+from tqdm.auto import tqdm
 from sklearn.model_selection import train_test_split
+from imblearn.under_sampling import RandomUnderSampler
 
-# Read in file
-df = pd.read_csv('data/sample.csv')
+df = pd.read_csv('AI_Human.csv')
 
-# Define x and y
 X = df['text'].values
 y = df['generated'].values
 
-# Text cleaning
-# Convert all words to lower case
-X = [string.lower() for string in X]
+# discarding 90% of the data
+X, _, y, _ = train_test_split(X, y, test_size=0.9, random_state=42)
 
-# Remove \n
-X = [string.replace("\n", "") for string in X]
+X = [word.lower() for word in tqdm(X)]
 
-# How should we deal with words with apostrophes like "don't"
-# Remove special characters - do we want to or not?
-# Tokenization
-# Remove stop words?
-# Lemmatization
-# Vectorization
-# Possible creation of new features
+# add more preprocessing here
 
-# Split data into training, validation, and testing sets - 80/10/10
-X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.20, random_state=42)
-X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
-X_train = X_train.reshape((-1, 1))
-X_val = X_val.reshape((-1, 1))
-X_test = X_test.reshape((-1, 1))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Perform undersampling to address class imbalance
 rus = RandomUnderSampler(random_state=42)
-X_resampled, y_resampled = rus.fit_resample(X_train, y_train)
-
-
-# Model Training and Evaluation
+X_resampled, y_resampled = rus.fit_resample(X_train.reshape(-1, 1), y_train.reshape(-1, 1))
