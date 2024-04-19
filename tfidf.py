f4131476eea374fbde380315aa6387c96987bf9f
Michelle Wang <120347052+wangmichelle1@users.noreply.github.com>
2024-04-19 12:24:48 -0400

    Delete tfidf.py

--- tfidf.py
+++
@@ -1,7 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-vectorizer = TfidfVectorizer()
-X_train_tfidf = vectorizer.fit_transform(X_train)
-
-test_vectorizer = TfidfVectorizer(vocabulary=vectorizer.vocabulary_)
-X_test_tfidf = test_vectorizer.fit_transform(X_test)
